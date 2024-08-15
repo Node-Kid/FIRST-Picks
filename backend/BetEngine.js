a5c0a4686d2d1b89df1aae00fa2cef9cbeefce69
Trig_ <weiszhaarwill@gmail.com>
2024-08-15 17:02:52 -0400

    init

---
+++ backend/BetEngine.js
@@ -0,0 +1,5 @@
+class BetEngine {
+
+}
+
+export { BetEngine }
---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+const port = 80
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
